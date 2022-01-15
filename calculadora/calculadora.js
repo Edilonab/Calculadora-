53f7876c9a307deac681ce7dec1507c9d4641de7
Edilonab <90010997+Edilonab@users.noreply.github.com>
2022-01-15 19:40:33 -0300

    finalizando javascript

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,4 +1,12 @@
 var valor;
+var resultado;
 function botao(num){
     valor = document.calc.visor.value += num;
+}
+function reseta(){
+    document.calc.visor.value = '';
+}
+function calcular(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado;
 }
